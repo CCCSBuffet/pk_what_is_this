582f3517ad6c24bebcf222ee430413a8ef826544
Perry Kivolowitz <perry@kivolowitz.com>
2022-05-02 13:31:54 -0500

    added some comments to main2 and started the README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# pk_what_is_this
-A demonstration of what the THIS POINTER is and how it is used
+# What Is "this"
+
+This demonstration aims to explain the role and implementation of the "*this pointer*."
+
+When a method belonging to a class instance is called, the method operates upon the data members of exactly that instance. How does it know which instance to operate upon?
+
+The C++ compiler performs many slights of hand including the implementation of a hidden parameter to all non-static class methods. The hidden parameter is the *this pointer*.
--- main2.cpp
+++ main2.cpp
@@ -2,11 +2,23 @@
 
 using namespace std;
 
-extern "C" void * FooConstructor(const char * _s);
+// This plays the role of the Foo default constructor.
+extern "C" void * FooConstructor(const char * _s = nullptr);
+
+/*	This plays the role of the non-static method GetString() which returns
+	the string initialized in the constructor  (either the string supplied
+	in the constructor or "undefined").
+
+	While GetString() does NOT take a parameter explicitly, C++ passes the
+	"this pointer" as a hidden first  parameter.  Here we pass this param-
+	eter explicitly in a demonstration of  how the  "this pointer" is imp-
+	lemented.
+*/
+
 extern "C" const char * FooGetString(const void *);
 
 int main() {
-	void * a = FooConstructor(nullptr);
+	void * a = FooConstructor();
 	void * b = FooConstructor("Spaghetti");
 
 	printf("a(%s)\n", FooGetString(a));
