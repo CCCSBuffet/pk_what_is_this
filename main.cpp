af93c5d90f5cfde30287ca36771d4c7e5a8c6242
Perry Kivolowitz <perry@kivolowitz.com>
2023-05-17 09:51:31 -0500

    this

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <cassert>
+#include <stdexcept>
 
 using namespace std;
 
@@ -6,6 +8,7 @@ class Foo {
 public:
 	Foo(const char * _s = nullptr);
 	const char * GetString();
+	static const char * StaticGetString(const Foo * _this);
 private:
 	const char * s;
 	static const char * undefined_string;
@@ -13,7 +16,16 @@ private:
 
 const char * Foo::undefined_string = (const char *) "undefined";
 
+const char * Foo::StaticGetString(const Foo * _this) {
+	if (!_this)
+		throw new invalid_argument(__FUNCTION__);
+	return _this->s;
+}
+
 Foo::Foo(const char * _s) {
+	// This is dangerous programming practice. Why?
+	// a) C strings in general are dangerous since they are dependent upon NULL termination.
+	// b) what else? 
 	s = (_s) ? _s : undefined_string;
 }
 
